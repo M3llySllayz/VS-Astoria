b091c09e76e31d319f5beff9e39b651b37aac563
Melly <78555659+M3llySllayz@users.noreply.github.com>
2022-07-19 19:00:02 -0400

    Finished Pixel

---
+++ example_mods/data/pixel/countdown.lua
@@ -0,0 +1,6 @@
+--if you're wondering where the meme 'Go!' comes from
+--it's in the actual code, you won't find it here
+--sorry! i tried to put it here, but stupid Arrays and Strings crashed the game
+function onCreate()
+    setProperty('introSoundsSuffix', '-meme')
+end
